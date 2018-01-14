d767e88e6e23805c6da090bad6dc828295735db5
chance <chance.a.cyphers@gmail.com>
2018-01-14 15:11:33 -0500

    Cleanup unused code

--- src/main/java/chance/pants/api/RedisConfiguration.java
+++ src/main/java/chance/pants/api/RedisConfiguration.java
@@ -7,14 +7,11 @@ import org.redisson.config.SingleServerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 @Configuration
 public class RedisConfiguration {
 
     @Bean
-    public RedissonClient redissonClient() throws URISyntaxException {
+    public RedissonClient redissonClient() {
         Config config = new Config();
         SingleServerConfig singleServerConfig = config.useSingleServer();
         String address = "redis://h:p274c0bf76333d63bf53c25d6d93abeda23afbf43f175b8ca24e447b0fe84da2f@ec2-54-85-101-85.compute-1.amazonaws.com:56199";
