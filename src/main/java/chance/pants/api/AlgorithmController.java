1589ab875112ef4716ea6ac3ac3b2b4c234c8407
chance <chance.a.cyphers@gmail.com>
2018-01-14 17:46:50 -0500

    Add get stop endpoint and edit redisson config for heroku

--- src/main/java/chance/pants/api/AlgorithmController.java
+++ src/main/java/chance/pants/api/AlgorithmController.java
@@ -1,11 +1,13 @@
 package chance.pants.api;
 
+import org.redisson.api.RBucket;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
@@ -19,6 +21,12 @@ public class AlgorithmController {
     public String createStop(@RequestBody String input) {
         redissonClient.getBucket("stop").set(input);
         return input;
+    }
+
+    @RequestMapping(method=GET)
+    public String getStop() {
+        RBucket<String> stopBucket = redissonClient.getBucket("stop");
+        return stopBucket.get();
     }
 
 }
--- src/main/java/chance/pants/api/RedisConfiguration.java
+++ src/main/java/chance/pants/api/RedisConfiguration.java
@@ -2,20 +2,39 @@ package chance.pants.api;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.RedisClient;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Configuration
 public class RedisConfiguration {
 
+//    @Bean
+//    public RedissonClient redissonClient() {
+//        Config config = new Config();
+//        SingleServerConfig singleServerConfig = config.useSingleServer();
+//        singleServerConfig.setAddress("redis://ec2-54-85-101-85.compute-1.amazonaws.com:56199");
+//        singleServerConfig.setPassword("p274c0bf76333d63bf53c25d6d93abeda23afbf43f175b8ca24e447b0fe84da2f");
+//        return Redisson.create(config);
+//    }
+
     @Bean
-    public RedissonClient redissonClient() {
+    public RedissonClient redissonClient() throws URISyntaxException {
         Config config = new Config();
         SingleServerConfig singleServerConfig = config.useSingleServer();
-        String address = "redis://h:p274c0bf76333d63bf53c25d6d93abeda23afbf43f175b8ca24e447b0fe84da2f@ec2-54-85-101-85.compute-1.amazonaws.com:56199";
-        singleServerConfig.setAddress(System.getenv("REDIS_URL"));
+
+        URI redisURI = new URI(System.getenv("REDIS_URL"));
+        String password = redisURI.getUserInfo().split(":", 2)[1];
+        String address = redisURI.getScheme() + "://" + redisURI.getHost() + ":" + redisURI.getPort();
+
+        singleServerConfig.setAddress(address);
+        singleServerConfig.setPassword(password);
+
         return Redisson.create(config);
     }
 
