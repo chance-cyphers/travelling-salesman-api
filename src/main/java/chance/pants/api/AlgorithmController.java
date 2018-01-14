9ac3541b531fdff654f6a468c7ce6bfaa142531e
chance <chance.a.cyphers@gmail.com>
2018-01-14 15:11:02 -0500

    Add redisson to project

--- src/main/java/chance/pants/api/AlgorithmController.java
+++ src/main/java/chance/pants/api/AlgorithmController.java
@@ -1,5 +1,8 @@
 package chance.pants.api;
 
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +12,13 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RequestMapping("/stop")
 public class AlgorithmController {
 
+    @Autowired
+    RedissonClient redissonClient;
+
     @RequestMapping(method=POST)
-    public String createStop() {
-        return "Hello world";
+    public String createStop(@RequestBody String input) {
+        redissonClient.getBucket("stop").set(input);
+        return input;
     }
 
 }
