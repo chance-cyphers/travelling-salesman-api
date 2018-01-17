e680967a085e67919b725abeae851b08620fc970
chance <chance.a.cyphers@gmail.com>
2018-01-16 22:18:18 -0500

    Allow CORS

--- src/main/java/chance/pants/api/Application.java
+++ src/main/java/chance/pants/api/Application.java
@@ -2,9 +2,23 @@ package chance.pants.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
