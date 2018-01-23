3c561e1146ae2dd924a49f356f54745a05370f38
chance <chance.a.cyphers@gmail.com>
2018-01-22 23:14:31 -0500

    Add logs for websocket

--- src/main/java/chance/pants/api/StockWebSocketHandler.java
+++ src/main/java/chance/pants/api/StockWebSocketHandler.java
@@ -1,5 +1,6 @@
 package chance.pants.api;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.socket.CloseStatus;
@@ -9,26 +10,31 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class StockWebSocketHandler extends TextWebSocketHandler {
 
+    public static final Logger logger = LoggerFactory.getLogger(StockWebSocketHandler.class);
     private BroadcastHandler broadcastHandler;
 
     @Autowired
     public StockWebSocketHandler(BroadcastHandler broadcastHandler) {
+        logger.debug("Got handler :" + broadcastHandler.toString());
         this.broadcastHandler = broadcastHandler;
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        logger.debug("Adding session :" + session.getId());
         broadcastHandler.add(session);
         super.afterConnectionEstablished(session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        logger.debug("Removing session :" + session.getId());
         broadcastHandler.remove(session);
         super.afterConnectionClosed(session, status);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        logger.debug("GOT INIT MESSAGE: " + message.getPayload());
     }
 }
