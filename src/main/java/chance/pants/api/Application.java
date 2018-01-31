c9841affd1bc0c34c707825f1fa79b3f7580fb73
chance <chance.a.cyphers@gmail.com>
2018-01-30 21:04:09 -0500

    Debugging websockets

--- src/main/java/chance/pants/api/Application.java
+++ src/main/java/chance/pants/api/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 import org.springframework.web.socket.handler.PerConnectionWebSocketHandler;
 
 @SpringBootApplication
+@Controller
 public class Application extends SpringBootServletInitializer implements WebSocketConfigurer {
 
     @Bean
@@ -43,7 +45,6 @@ public class Application extends SpringBootServletInitializer implements WebSock
 
 
     public static void main(String[] args) {
-        System.out.println("main happens, at least");
         SpringApplication.run(Application.class, args);
     }
 
