c1e198a772cd9388fe4bf4ac456450ff9ed8ebe6
chance <chance.a.cyphers@gmail.com>
2018-02-24 14:08:06 -0500

    Register with eureka

--- src/main/java/chance/pants/api/Application.java
+++ src/main/java/chance/pants/api/Application.java
@@ -3,6 +3,7 @@ package chance.pants.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Controller
 public class Application extends SpringBootServletInitializer {
 
